d4c4da4a93e9e7e897e79d427477054b6f7d8068
JackLiu3 <86380806+JackLiu3@users.noreply.github.com>
2021-07-27 22:58:32 -0500

    handleComplete, .map, .filter

--- src/App.js
+++ src/App.js
@@ -12,25 +12,64 @@ export default function App() {
     HideComplete(boolean)
   */
   const [taskNameToAdd, setTaskNameToAdd] = useState("");
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState([
+    {
+      finished: false,
+      taskName: "Have Dinner with Dad",
+      due_date: ""
+    },
+    {
+      finished: false,
+      taskName: "Eat some fruits",
+      due_date: ""
+    },
+    {
+      finished: true,
+      taskName: "Drink some water",
+      due_date: ""
+    },
+    {
+      finished: false,
+      taskName:
+        "Learn react and finish the todo-list app, and the challenge in FFC.",
+      due_date: ""
+    }
+  ]);
   const [hideComplete, setHideComplete] = useState(false);
   const handleChange = (e) => {
     setTaskNameToAdd(e.target.value);
   };
   const handleChangeInHc = () => {
-    if (hideComplete === true) {
-      setHideComplete(false);
-    } else {
-      setHideComplete(true);
+    setHideComplete(!hideComplete);
+  };
+  const handleAdd = () => {
+    let obj = {
+      finished: false,
+      taskName: taskNameToAdd,
+      due_date: ""
+    };
+    setTaskList([obj].concat(taskList));
+  };
+  const handleComplete = (e) => {
+    //todo update todo list after handleComplete
+    console.log("complete " + e.target.value);
+    let theItem = taskList.find((item) => item.taskName === e.target.value);
+    if (theItem !== undefined) {
+      theItem.finished = true;
     }
   };
+
   return (
     <div className="todo-app">
       <h2>Todo List</h2>
       <div className="imcomplete-banner">
         <div className="imcomplete-number">3 tasks to complete</div>
         <div className="hide-complete">
-          <input type="checkbox" onClick={handleChangeInHc} />
+          <input
+            type="checkbox"
+            onChange={handleChangeInHc}
+            checked={hideComplete}
+          />
           Hide Complete
         </div>
       </div>
@@ -38,32 +77,25 @@ export default function App() {
       <hr />
 
       <div className="task-list">
-        <div className="task-line">
-          <div className="task-checkbox">
-            <input type="checkbox" />
-          </div>
-          <div className="task-content">Have Dinner with Dad</div>
-        </div>
-        <div className="task-line">
-          <div className="task-checkbox">
-            <input type="checkbox" />
-          </div>
-          <div className="task-content">Eat some fruits</div>
-        </div>
-        <div className="task-line">
-          <div className="task-checkbox">
-            <input type="checkbox" checked />
-          </div>
-          <div className="task-content">Drink some water</div>
-        </div>
-        <div className="task-line">
-          <div className="task-checkbox">
-            <input type="checkbox" />
-          </div>
-          <div className="task-content">
-            Learn react and finish the todo-list app, and the challenge in FFC.
-          </div>
-        </div>
+        {taskList
+          .filter((item, index) => {
+            return hideComplete ? !item.finished : true;
+          })
+          .map((item, index) => {
+            return (
+              <div className="task-line" key={index}>
+                <div className="task-checkbox">
+                  <input
+                    type="checkbox"
+                    value={item.taskName}
+                    onChange={handleComplete}
+                    checked={item.finished}
+                  />
+                </div>
+                <div className="task-content">{item.taskName}</div>
+              </div>
+            );
+          })}
       </div>
       <hr />
 
@@ -75,7 +107,9 @@ export default function App() {
           value={taskNameToAdd}
           onChange={handleChange}
         />{" "}
-        <button className="add-button">Add it</button>
+        <button className="add-button" onClick={handleAdd}>
+          Add it
+        </button>
       </div>
     </div>
   );
