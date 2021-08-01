7fd49c7dd1b85d836a048374ff8f72f838470cdf
JackLiu3 <86380806+JackLiu3@users.noreply.github.com>
2021-08-01 14:04:47 -0500

    'small' bug

--- src/api/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "../reducers/todoSlice";
-
-export default configureStore({
-  reducer: {
-    todo: todoReducer
-  }
-});
