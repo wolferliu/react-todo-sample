4ccd1332d60ec5f07a038db348044cb9fb082269
Liu Bing <wolfer.liu@gmail.com>
2021-08-03 10:03:56 +0800

    fix the link to todoSlice

--- src/api/store.js
+++ src/api/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "../reducers/todoSlice";
+import todoReducer from "../features/todo/todoSlice";
 
 export default configureStore({
   reducer: {
